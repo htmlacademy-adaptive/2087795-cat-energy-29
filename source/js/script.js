e6247902765bab906c112cd0595406450c6679bf
TashaTul <natu.tulyakova@gmail.com>
2023-09-01 14:59:02 +0300

    Выполняет стилизацию страницы form

--- source/js/script.js
+++ source/js/script.js
@@ -16,11 +16,11 @@
 const navMain = document.querySelector('.main-header__wrapper');
 const navToggle = document.querySelector('.main-header__toggle');
 
-// Убирает класс main-header__nav--nojs
+// Убирает класс main-header__wrapper--nojs
 
-navMain.classList.remove('main-header__nav--nojs');
+navMain.classList.remove('main-header__wrapper--nojs');
 
 navToggle.addEventListener('click', function () {
-  navMain.classList.toggle('main-header__nav--closed');
-  navMain.classList.toggle('main-header__nav--opened');
+  navMain.classList.toggle('main-header__wrapper--closed');
+  navMain.classList.toggle('main-header__wrapper--opened');
 });
