2946c000c5e305550aaf99d4547dd9aedc354dba
TashaTul <natu.tulyakova@gmail.com>
2023-09-14 14:54:34 +0300

    Вносит правки в мобильное меню2

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,13 @@
-// const navMain = document.querySelector('.main-header__wrapper');
-// const navToggle = document.querySelector('.main-header__toggle');
+const navMain = document.querySelector('.main-header__wrapper');
+const navToggle = document.querySelector('.main-header__toggle');
 
-// // Убирает класс main-header__wrapper--nojs
+// Убирает класс main-header__wrapper--nojs
 
-// navMain.classList.remove('main-header__wrapper--nojs');
-// navMain.classList.remove('main-header__wrapper--opened');
-// navMain.classList.add('main-header__wrapper--closed');
+navMain.classList.remove('main-header__wrapper--nojs');
+navMain.classList.remove('main-header__wrapper--opened');
+navMain.classList.add('main-header__wrapper--closed');
 
-// navToggle.addEventListener('click', function () {
-//   navMain.classList.toggle('main-header__wrapper--closed');
-//   navMain.classList.toggle('main-header__wrapper--opened');
-// });
+navToggle.addEventListener('click', function () {
+  navMain.classList.toggle('main-header__wrapper--closed');
+  navMain.classList.toggle('main-header__wrapper--opened');
+});
